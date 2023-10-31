2eb03238e608a54b8e4d1eb2dfdd6203e006d9e6
talha <talhaaly103@gmail.com>
2023-10-31 12:52:59 +0500

    StartJs

---
+++ app.js
@@ -0,0 +1,53 @@
+const questions = [
+    {
+        question: "which is largest animal in the world",
+        answers: [
+            {text: "shark", correct: "false"},
+            {text: "Blue Whale", correct: "true"},
+            {text: "Elephant", correct: "false"},
+            {text: "Giraffe", correct: "false"},
+        ]
+    },
+
+    {
+        question: "which is largest animal in the world",
+        answers: [
+            {text: "shark", correct: "false"},
+            {text: "Blue Whale", correct: "true"},
+            {text: "Elephant", correct: "false"},
+            {text: "Giraffe", correct: "false"},
+        ]
+    },
+
+    {
+        question: "which is largest animal in the world",
+        answers: [
+            {text: "shark", correct: "false"},
+            {text: "Blue Whale", correct: "true"},
+            {text: "Elephant", correct: "false"},
+            {text: "Giraffe", correct: "false"},
+        ]
+    },
+    {
+        question: "which is largest animal in the world",
+        answers: [
+            {text: "shark", correct: "false"},
+            {text: "Blue Whale", correct: "true"},
+            {text: "Elephant", correct: "false"},
+            {text: "Giraffe", correct: "false"},
+        ]
+    },
+    {
+        question: "which is largest animal in the world",
+        answers: [
+            {text: "shark", correct: "false"},
+            {text: "Blue Whale", correct: "true"},
+            {text: "Elephant", correct: "false"},
+            {text: "Giraffe", correct: "false"},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question")
+const answerButton = document.getElementById("answer-buttons")
+const next = document.getElementById("next-btn")
