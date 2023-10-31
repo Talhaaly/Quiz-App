8de06c7903276fdbb3c11a0b431a40563bc42eb6
talha <talhaaly103@gmail.com>
2023-10-31 15:10:56 +0500

    compelete

--- app.js
+++ app.js
@@ -1,51 +1,96 @@
 const questions = [
     {
-        question: "which is largest animal in the world",
+        question: "What does HTML stand for?",
         answers: [
-            { text: "shark", correct: "false" },
-            { text: "Blue Whale", correct: "true" },
-            { text: "Elephant", correct: "false" },
-            { text: "Giraffe", correct: "false" },
+            { text: "How to Make Lasagnaale", correct: "false" },
+            { text: "Hyper Text Markup Language", correct: "true" },
+            { text: "Hot Mail", correct: "false" },
+            { text: "Hyper Texts Makeup Language", correct: "false" },
         ]
     },
 
     {
-        question: "which is largest animal in the world",
+        question: "How many tags are in a regular element?",
         answers: [
-            { text: "shark", correct: "false" },
-            { text: "Blue Whale", correct: "true" },
-            { text: "Elephant", correct: "false" },
-            { text: "Giraffe", correct: "false" },
+            { text: "2", correct: "true" },
+            { text: "3", correct: "false" },
+            { text: "1", correct: "false" },
+            { text: "4", correct: "false" },
         ]
     },
 
     {
-        question: "which is largest animal in the world",
+        question: "What is the difference between an opening tag and a closing tag?",
         answers: [
-            { text: "shark", correct: "false" },
-            { text: "Blue Whale", correct: "true" },
-            { text: "Elephant", correct: "false" },
-            { text: "Giraffe", correct: "false" },
+            { text: "There is no difference", correct: "false" },
+            { text: "Opening tag has a / in front", correct: "false" },
+            { text: "Closing tag has a / in front", correct: "true" },
+            { text: "None Of This", correct: "false" },
         ]
     },
     {
-        question: "which is largest animal in the world",
+        question: "< br  / > What type of tag is this?",
         answers: [
-            { text: "shark", correct: "false" },
-            { text: "Blue Whale", correct: "true" },
-            { text: "Elephant", correct: "false" },
-            { text: "Giraffe", correct: "false" },
+            { text: "Break tag", correct: "true" },
+            { text: "None Of This", correct: "false" },
+            { text: "A broken one", correct: "false" },
+            { text: "An opening tag", correct: "false" },
         ]
     },
     {
-        question: "which is largest animal in the world",
+        question: "Where is the meta tag only found?",
         answers: [
-            { text: "shark", correct: "false" },
-            { text: "Blue Whale", correct: "true" },
-            { text: "Elephant", correct: "false" },
-            { text: "Giraffe", correct: "false" },
+            { text: "The last page", correct: "false" },
+            { text: "None Of This", correct: "false" },
+            { text: "The second page", correct: "false" },
+            { text: "The home page", correct: "true" },
         ]
-    }
+    },
+    {
+        question: " Which is the correct way to tag an image?",
+        answers: [
+            { text: `src=”image.jpg/gif” alt=”type some text”`, correct: "false" },
+            { text: `img src="image.jpg/gif" alt=img"`, correct: "true" },
+            { text: `Src=”image.jpg/gif” alt=type some text”`, correct: "false" },
+            { text: `Src=”img.jpg” alt=image”`, correct: "false" },
+        ]
+    },
+    {
+        question: "What is an element that does not have a closing tag called?",
+        answers: [
+            { text: "Tag", correct: "false" },
+            { text: "None Of This", correct: "false" },
+            { text: "Empty element", correct: "true" },
+            { text: "Closed element", correct: "false" },
+        ]
+    },
+    {
+        question: " What should values always be enclosed in?",
+        answers: [
+            { text: "Parenthesis", correct: "false" },
+            { text: "None Of This", correct: "false" },
+            { text: "Quotation marks", correct: "true" },
+            { text: "Commas", correct: "false" },
+        ]
+    },
+    {
+        question: "How can you add space between the border and inner content of the element?",
+        answers: [
+            { text: "margin", correct: "false" },
+            { text: "border", correct: "false" },
+            { text: "Spacing", correct: "false" },
+            { text: "padding", correct: "true" },
+        ]
+    },
+    {
+        question: "What does CSS stand for?",
+        answers: [
+            { text: "Computer Style Sheet", correct: "false" },
+            { text: "Colorful Style Sheet", correct: "false" },
+            { text: "None Of This", correct: "true" },
+            { text: "Creative Style Sheet", correct: "false" },
+        ]
+    },
 ];
 
 const questionElement = document.getElementById("question");
@@ -117,19 +162,19 @@ function showScore() {
 
 function handleNextButton() {
     currentQuestionIndex++;
-    if(currentQuestionIndex < questions.length) {
+    if (currentQuestionIndex < questions.length) {
         showQuestion();
-    }else{
+    } else {
         showScore();
     }
 }
 
 
 
-nextButton.addEventListener("click", ()=>{
-    if(currentQuestionIndex < questions.length){
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
         handleNextButton();
-    }else {
+    } else {
         startQuiz();
     }
 })
